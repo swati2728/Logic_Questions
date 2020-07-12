e2682828a38cc8c48459f16c6848f34c903b39b7
Swati2728 <swati19@navgurukul.org>
2020-07-12 20:28:35 +0530

    seperated the file

--- basic.py
+++ basic.py
@@ -1,30 +1,3 @@
-# var_1=45
-# b=2.1
-# c="swati"
-# print(var_1,b,c)
-# print(type(var_1))
-# print(type(b))
-# print(type(c))
-
-# a = b = c = 1
-# print(a,b,c)
-
-# a,b,c = 1,2,"john"
-# print(a,b,c)
-
-# str = 'Hello World!'
-
-# print (str)          
-# print (str[0])       
-# print (str[2:5])     
-# print (str[2:])      
-# print (str * 2 )     
-# print (str + "TEST") 
-
-# gurukul="swati"
-# var_nav=2.5
-# print(gurukul,var_nav)
-
 # ============================================================================
 
 # conversion
